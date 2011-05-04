d4a9c0d9a13b986f41f1422169b902ea0ba495e4
marcetsii <marcetsii@2efce2e8-4f2f-539b-da39-26229b73ec71>
2011-05-04 23:03:52 +0000

    Metodos SELECT de EncuestaDAO y preguntaDAO terminados y Testeados de la BD, todo OK.

--- FomentoSoftwareLibre/src/test/TestEncuestaJDBC.java
+++ FomentoSoftwareLibre/src/test/TestEncuestaJDBC.java
@@ -1,11 +1,16 @@
 package test;
 
-import com.mysql.jdbc.JDBC4CallableStatement;
+import java.util.LinkedList;
+import java.util.List;
 
 import pos.data.IEncuestaDAO;
+import pos.data.IPreguntaDAO;
 import pos.data.JDBCEncuestaDAO;
+import pos.data.JDBCPreguntaDAO;
 import pos.domain.Encuesta;
 import pos.domain.EncuestaImpl;
+import pos.domain.Pregunta;
+import pos.domain.PreguntaImpl;
 
 public class TestEncuestaJDBC {
 
@@ -15,8 +20,32 @@ public class TestEncuestaJDBC {
 	public static void main(String[] args) {
 		Encuesta e = new EncuestaImpl();
 		IEncuestaDAO edao = new JDBCEncuestaDAO();
+		List<Encuesta> le = new LinkedList<Encuesta>();
+		Pregunta p = new PreguntaImpl();
+		List<Pregunta> lp = new LinkedList<Pregunta>();
+		IPreguntaDAO pdao = new JDBCPreguntaDAO();
+		
+		//recuperar encuesta
 		e=edao.recuperarEncuesta(1);
-		System.out.print(e.getTituloEncuesta());
+		System.out.println(e.getTituloEncuesta());
+		
+		//lista de todas las encuestas
+		le = edao.seleccionarTodasEncuestas();
+		for (Encuesta enc :le){
+			System.out.println(enc.getTituloEncuesta());
+		}
+		
+		//recuperarPregunta
+		
+		p=pdao.recuperarPregunta(1);
+		System.out.println(p.getEnunciado());
+		
+		//preguntas de una encuesta
+		lp = pdao.seleccionarTodasPreguntasPorEncuesta(1);
+		for (Pregunta pr :lp){
+			System.out.println(pr.getEnunciado());
+		}
+		
 	}
 
 }
