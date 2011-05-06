fb6beacd556a3a8256b85209344f7c68075fc964
fragelbreak <fragelbreak@2efce2e8-4f2f-539b-da39-26229b73ec71>
2011-05-06 20:07:07 +0000

    Proyecto y ProyectoImpl creados e implementados de nuevo. DAOS Creados y semi implementados

---
+++ trunk/FomentoSoftwareLibre/src/pos/domain/ProyectoImpl.java
@@ -0,0 +1,74 @@
+package pos.domain;
+
+import java.util.Date;
+
+public class ProyectoImpl implements Proyecto {
+	
+	private Integer idProyecto;
+	private String nombreProyecto;
+	private String descripcion;
+	private Date fechaInicio;
+	private Date fechaFin;
+	private Boolean disponibilidad;
+	private Integer numeroMaximoDesarrolladores;
+	private Integer numeroActualDesarrolladores;
+	
+	
+	/*
+	 * MÉTODOS GETTERS Y SETTERS
+	 */
+	
+	
+	
+	public String getNombreProyecto() {
+		return nombreProyecto;
+	}
+	public void setNombreProyecto(String nombreProyecto) {
+		this.nombreProyecto = nombreProyecto;
+	}
+	public String getDescripcionProyecto() {
+		return descripcion;
+	}
+	public void setDescripcionProyecto(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	public Boolean getDisponibilidad() {
+		return disponibilidad;
+	}
+	public void setDisponibilidad(Boolean disponibilidad) {
+		this.disponibilidad = disponibilidad;
+	}
+	public Integer getNumeroMaximoDesarrolladores() {
+		return numeroMaximoDesarrolladores;
+	}
+	public void setNumeroMaximoDesarrolladores(Integer numeroMaximoDesarrolladores) {
+		this.numeroMaximoDesarrolladores = numeroMaximoDesarrolladores;
+	}
+	public Integer getNumeroActualDesarrolladores() {
+		return numeroActualDesarrolladores;
+	}
+	public void setNumeroActualDesarrolladores(Integer numeroActualDesarrolladores) {
+		this.numeroActualDesarrolladores = numeroActualDesarrolladores;
+	}
+	public Integer getIDProyecto() {
+		return idProyecto;
+	}
+	@Override
+	public void setIDProyecto(Integer idProyecto) {
+		this.idProyecto = idProyecto;
+	}
+
+	
+}
