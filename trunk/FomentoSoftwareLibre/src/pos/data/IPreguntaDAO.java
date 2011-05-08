c0ad6b166fdf0ff7d4070a112e9c0765db4259ea
marcetsii <marcetsii@2efce2e8-4f2f-539b-da39-26229b73ec71>
2011-05-08 09:57:33 +0000

    Borrado de preguntas y respuestas por ID concluido y probado. Falta borrar una encuesta

--- trunk/FomentoSoftwareLibre/src/pos/data/IPreguntaDAO.java
+++ trunk/FomentoSoftwareLibre/src/pos/data/IPreguntaDAO.java
@@ -5,8 +5,8 @@ import java.util.List;
 import pos.domain.Pregunta;
 
 public interface IPreguntaDAO {
-    public void borrar(String PreguntaID);
     public List<Pregunta> seleccionarTodasPreguntasPorEncuesta(Integer EncuestaId);
     public void insertarPregunta(Pregunta p,Integer idEncuesta);
     public Pregunta recuperarPregunta(Integer idPregunta);
+	void borrar(Integer preguntaID);
 }
--- trunk/FomentoSoftwareLibre/src/pos/data/IRespuestaDAO.java
+++ trunk/FomentoSoftwareLibre/src/pos/data/IRespuestaDAO.java
@@ -5,9 +5,9 @@ import java.util.List;
 import pos.domain.Respuesta;
 
 public interface IRespuestaDAO {
-    public void borrar(String RespuestaID);
     public List<Respuesta> seleccionarTodasRespuestasPorPregunta(Integer IdPregunta);
     public void insertarRespuesta(Integer rID,Respuesta r);
     public Respuesta recuperarRespuesta(Integer idRespuesta);
+	void borrar(Integer RespuestaID);
 	
 }
