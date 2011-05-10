66977a6f968dfd9ef4ee6bf8853c61d8b6b67aa0
lordreivaj <lordreivaj@2efce2e8-4f2f-539b-da39-26229b73ec71>
2011-05-10 17:10:47 +0000

    Ahora al borrar un voto quita su valor en los contadores de la aplicacion

--- trunk/FomentoSoftwareLibre/src/pos/data/JDBCVotarDAO.java
+++ trunk/FomentoSoftwareLibre/src/pos/data/JDBCVotarDAO.java
@@ -13,12 +13,12 @@ import pos.domain.Voto;
 import pos.domain.VotoImpl;
 
 public class JDBCVotarDAO implements IVotarDAO {
-	
-	 private Connection conn;
-	 
-	 public JDBCVotarDAO (){
-		 conn = ConnectionManager.getInstance().checkOut();
-	 }
+
+	private Connection conn;
+
+	public JDBCVotarDAO (){
+		conn = ConnectionManager.getInstance().checkOut();
+	}
 
 	@Override
 	public List<Voto> selectAll() {
@@ -26,22 +26,22 @@ public class JDBCVotarDAO implements IVotarDAO {
 		PreparedStatement stmt = null;
 		ResultSet result = null;
 		List<Voto> res = null;
-		
+
 		try {
 			stmt = conn.prepareStatement(sql);
 
 			result = stmt.executeQuery();
 			res = new LinkedList<Voto>();
-			
+
 			while(result.next()){
-			Voto voto = new VotoImpl();
-			voto.setUsuario(result.getInt("IDUsuario"));
-			voto.setAplicacion(result.getInt("IDAplicacion"));
-			voto.setValor(result.getBoolean("valor"));
-			res.add(voto);
+				Voto voto = new VotoImpl();
+				voto.setUsuario(result.getInt("IDUsuario"));
+				voto.setAplicacion(result.getInt("IDAplicacion"));
+				voto.setValor(result.getBoolean("valor"));
+				res.add(voto);
 			}
-			
-			
+
+
 		} catch (SQLException e) {
 			System.out.println("Message: " + e.getMessage());
 			System.out.println("SQLState: " + e.getSQLState());
@@ -67,23 +67,23 @@ public class JDBCVotarDAO implements IVotarDAO {
 		PreparedStatement stmt = null;
 		ResultSet result = null;
 		List<Voto> res = null;
-		
+
 		try {
 			stmt = conn.prepareStatement(sql);
 			stmt.setInt(1, idUser);
 
 			result = stmt.executeQuery();
 			res = new LinkedList<Voto>();
-			
+
 			while(result.next()){
-			Voto voto = new VotoImpl();
-			voto.setUsuario(result.getInt("IDUsuario"));
-			voto.setAplicacion(result.getInt("IDAplicacion"));
-			voto.setValor(result.getBoolean("valor"));
-			res.add(voto);
+				Voto voto = new VotoImpl();
+				voto.setUsuario(result.getInt("IDUsuario"));
+				voto.setAplicacion(result.getInt("IDAplicacion"));
+				voto.setValor(result.getBoolean("valor"));
+				res.add(voto);
 			}
-			
-			
+
+
 		} catch (SQLException e) {
 			System.out.println("Message: " + e.getMessage());
 			System.out.println("SQLState: " + e.getSQLState());
@@ -109,23 +109,23 @@ public class JDBCVotarDAO implements IVotarDAO {
 		PreparedStatement stmt = null;
 		ResultSet result = null;
 		List<Voto> res = null;
-		
+
 		try {
 			stmt = conn.prepareStatement(sql);
 			stmt.setInt(1, Aplicacion);
 
 			result = stmt.executeQuery();
 			res = new LinkedList<Voto>();
-			
+
 			while(result.next()){
-			Voto voto = new VotoImpl();
-			voto.setUsuario(result.getInt("IDUsuario"));
-			voto.setAplicacion(result.getInt("IDAplicacion"));
-			voto.setValor(result.getBoolean("valor"));
-			res.add(voto);
+				Voto voto = new VotoImpl();
+				voto.setUsuario(result.getInt("IDUsuario"));
+				voto.setAplicacion(result.getInt("IDAplicacion"));
+				voto.setValor(result.getBoolean("valor"));
+				res.add(voto);
 			}
-			
-			
+
+
 		} catch (SQLException e) {
 			System.out.println("Message: " + e.getMessage());
 			System.out.println("SQLState: " + e.getSQLState());
@@ -148,23 +148,23 @@ public class JDBCVotarDAO implements IVotarDAO {
 	@Override
 	public void insertVoto(Voto voto) {
 		String sql = "INSERT INTO votos(IDUsuario, IDAplicacion, valor) " +
-				"VALUES ( ?, ?, ?)" ;
-		
+		"VALUES ( ?, ?, ?)" ;
+
 		PreparedStatement stmt1 = null;
 		ResultSet result1 = null;
-		
+
 		try {
 			stmt1 = conn.prepareStatement(sql);
 			stmt1.setInt(1, voto.getUsuario());
 			stmt1.setInt(2, voto.getAplicacion());
 			stmt1.setBoolean(3, voto.getValor());
-						
+
 			stmt1.executeUpdate();
-			
-			Aplicacion apli = getAplicacion(voto);
-			updateAplicacion(voto, apli);
-			
-			
+
+			Aplicacion apli = getAplicacion(voto.getAplicacion());
+			updateAplicacion(voto, apli, 1);
+
+
 		} catch (SQLException e) {
 			System.out.println("Message: " + e.getMessage());
 			System.out.println("SQLState: " + e.getSQLState());
@@ -187,12 +187,18 @@ public class JDBCVotarDAO implements IVotarDAO {
 		String sql = "DELETE FROM votos WHERE IDVoto = ?";
 		PreparedStatement stmt = null;
 		ResultSet result = null;
-		
+
 		try {
 			stmt = conn.prepareStatement(sql);
 			stmt.setInt(1, voto);
-						
+			
+			
+			Voto res = getVoto(voto);
+			Aplicacion apli = getAplicacion(res.getAplicacion());
+			updateAplicacion(res, apli, -1);
+			
 			stmt.executeUpdate();
+			
 		} catch (SQLException e) {
 			System.out.println("Message: " + e.getMessage());
 			System.out.println("SQLState: " + e.getSQLState());
@@ -210,28 +216,27 @@ public class JDBCVotarDAO implements IVotarDAO {
 		}
 	}
 	
-	private Aplicacion getAplicacion(Voto voto){
-		String sq3 = "Select * FROM aplicaciones WHERE IDAplicacion = ?";
-		
+// ------------------------------------- X -------------------------------
+	
+	
+	private Aplicacion getAplicacion(Integer aplicacion){
+		String sq = "Select * FROM aplicaciones WHERE IDAplicacion = ?";
+
 		PreparedStatement stmt = null;
 		ResultSet result = null;
-		
+
 		Aplicacion apli = new AplicacionImpl();
-		
+
 		try {
-			stmt = conn.prepareStatement(sq3);
-			stmt.setInt(1, voto.getAplicacion());
+			stmt = conn.prepareStatement(sq);
+			stmt.setInt(1, aplicacion);
 			result = stmt.executeQuery();
-			
+
 			result.next();
-			
-			apli.setNombre(result.getString("nombre"));
-			apli.setDescripcion(result.getString("descripcion"));
-			apli.setFechaPublicacion(result.getDate("fechaPublicacion"));
-			apli.setURLWeb(result.getString("URLWeb"));
+
 			apli.setVotosAFavor(result.getInt("numeroVotosAFavor"));
 			apli.setVotosEnContra(result.getInt("numeroVotosEnContra"));
-						
+
 		} catch (SQLException e) {
 			System.out.println("Message: " + e.getMessage());
 			System.out.println("SQLState: " + e.getSQLState());
@@ -247,14 +252,13 @@ public class JDBCVotarDAO implements IVotarDAO {
 			} catch (SQLException e) {
 			}
 		}
-		
 		return apli;		
 	}
-	
-	private void updateAplicacion(Voto voto, Aplicacion apli){
+
+	private void updateAplicacion(Voto voto, Aplicacion apli, Integer i){
 		PreparedStatement stmt = null;
 		ResultSet result = null;
-		
+
 		String sq;
 		if(voto.getValor()){
 			sq = "UPDATE aplicaciones SET numeroVotosAFavor = ? " +
@@ -264,18 +268,18 @@ public class JDBCVotarDAO implements IVotarDAO {
 			"WHERE aplicaciones.IDAplicacion = ?";			
 		}
 		try {
-			
-		stmt = conn.prepareStatement(sq);
-		if(voto.getValor()){
-			stmt.setInt(1, apli.getVotosAFavor()+1);
-			
-		}else{
-			stmt.setInt(1, apli.getVotosEnContra()+1);
-		}
-		stmt.setInt(2, voto.getAplicacion());
-		
-		stmt.executeUpdate();				
-		
+
+			stmt = conn.prepareStatement(sq);
+			if(voto.getValor()){
+				stmt.setInt(1, apli.getVotosAFavor()+i);
+
+			}else{
+				stmt.setInt(1, apli.getVotosEnContra()+i);
+			}
+			stmt.setInt(2, voto.getAplicacion());
+
+			stmt.executeUpdate();				
+
 		} catch (SQLException e) {
 			System.out.println("Message: " + e.getMessage());
 			System.out.println("SQLState: " + e.getSQLState());
@@ -291,8 +295,41 @@ public class JDBCVotarDAO implements IVotarDAO {
 			} catch (SQLException e) {
 			}
 		}
-		
-		
 	}
+	
+	private Voto getVoto(Integer voto){
+		String sq = "Select * FROM votos WHERE IDVoto = ?";
 
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+
+		Voto res = new VotoImpl();
+
+		try {
+			stmt = conn.prepareStatement(sq);
+			stmt.setInt(1, voto);
+			result = stmt.executeQuery();
+
+			result.next();
+
+			res.setAplicacion(result.getInt("IDAplicacion"));
+			res.setValor(result.getBoolean("valor"));
+			
+		} catch (SQLException e) {
+			System.out.println("Message: " + e.getMessage());
+			System.out.println("SQLState: " + e.getSQLState());
+			System.out.println("ErrorCode: " + e.getErrorCode());
+		} finally {
+			try {
+				if (result != null) {
+					result.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (SQLException e) {
+			}
+		}
+		return res;		
+	}
 }
