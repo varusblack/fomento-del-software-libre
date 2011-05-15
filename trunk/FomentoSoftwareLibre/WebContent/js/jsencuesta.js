bbceaf60558e4bc3a82c8b93e115706d76c8a0a5
marcetsii <marcetsii@2efce2e8-4f2f-539b-da39-26229b73ec71>
2011-05-15 15:50:38 +0000

    Generador de código html conseguido :)

--- trunk/FomentoSoftwareLibre/WebContent/js/jsencuesta.js
+++ trunk/FomentoSoftwareLibre/WebContent/js/jsencuesta.js
@@ -1,6 +1,15 @@
-function addElement(texto){
+var n = 2;
+function addElement(){
 	var capa = document.getElementById("capa");
-	var h1 = document.createElement("h1");
-	h1.innerHTML = texto;
-	capa.appendChild(h1);
+	var label = document.createElement("label");
+	var texto = document.createElement("input");
+	label.innerHTML = "Respuesta "+n;
+	texto.type = "text";
+	texto.name = "resp"+n;
+	texto.id ="res"+n;
+	capa.appendChild(label);
+	capa.appendChild(texto);
+	capa.innerHTML+="<br/>";
+	n++;
+	
 } 
