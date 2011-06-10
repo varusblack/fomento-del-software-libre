dbc16f499c6104b7ebb1be3d613ad70142312bbd
marcetsii <marcetsii@2efce2e8-4f2f-539b-da39-26229b73ec71>
2011-06-10 16:51:53 +0000

    Machando

--- FomentoSoftwareLibre/src/servlets/ServletInsertarEncuesta.java
+++ FomentoSoftwareLibre/src/servlets/ServletInsertarEncuesta.java
@@ -47,16 +47,18 @@ public class ServletInsertarEncuesta extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	Encuesta encuesta = new EncuestaImpl();
     	Map<String,List<Respuesta>> mp = new HashMap<String,List<Respuesta>>();
+    	List<Pregunta> lpaux = new LinkedList<Pregunta>();
     	List<Pregunta> lp = new LinkedList<Pregunta>();
     	List<Respuesta>lr = new LinkedList<Respuesta>();
-    	Integer numpreg = new Integer(request.getParameter("num"));
 
     	
     	response.getWriter().println("HAS LOGRADO TENER CHANCE!!!!!! JAVA CHANCEADO CON JSP");
-    	response.getWriter().println(numpreg);
+    	//response.getWriter().println(numpreg);
 
     	Enumeration<?> e=request.getParameterNames(); 	
-
+    	
+    	//Recogida de información
+    	
     	while (e.hasMoreElements()){
     		String cad = (String) e.nextElement();
     		response.getWriter().println("Enumeration : "+cad);
@@ -76,18 +78,52 @@ public class ServletInsertarEncuesta extends HttpServlet {
     				response.getWriter().println("Valor "+lraux);
     				lraux.add(r);
     				mp.put(c1.toString(), lraux);
+    				response.getWriter().println("illo que "+lraux.get(lraux.size()-1).getDescripcionRespuesta());
     			}
     			else{
     				List<Respuesta> lraux = new LinkedList<Respuesta>();
     				lraux.add(r);
     				mp.put(c1.toString(), lraux);
-    				response.getWriter().println("illo que "+lraux.size());
     			}
-    			response.getWriter().println(cad+" "+mp.keySet());//+" "+mp.get("1").size());
+    			response.getWriter().println(cad+" "+mp.keySet());
     		}
+    		
     		if (cad.contains("pre")){
-    			
+    			Pregunta p = new PreguntaImpl();
+    			Character c = cad.charAt(cad.length()-1);//numero de pregunta
+    			p.setIDPregunta(c.toString());
+    			p.setEnunciado(request.getParameter(cad));
+    			lpaux.add(p);
+    			response.getWriter().println("enun "+lpaux);
+    		}
+    		
+    	}
+    	
+    	// agrupado de la información y ordenación.
+    	
+    	//ordenación de preguntas por inserción
+    	
+    	for (Integer i =1 ; i<=lpaux.size(); i++){
+    		for (Integer j =0; j< lpaux.size();j++){
+    			if (lpaux.get(j).getIDPregunta().equals(i.toString())){
+    				lp.add(lpaux.get(j));
+    				break;
+    			}
     		}
     	}
+    	
+    	//inserción de respuestas
+    	//System.out.print(mp.get(0).get(0).getDescripcionRespuesta());
+    	//MAPEO INCORRECTO
+    	
+    	for (Integer i =0; i <lpaux.size(); i ++){
+    		lr = mp.get(i.toString()); 
+    		lp.get(i).setRespuestas(lr);
+    	}
+    	
+    	/*for (Respuesta r : lp.get(0).getRespuestas()){
+    		response.getWriter().println("Respuestas "+r.getDescripcionRespuesta());
+    	}*/
+    	
     }
 }
